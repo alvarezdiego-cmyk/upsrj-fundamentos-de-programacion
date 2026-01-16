d059be0cbff2b95bd6c85710ed9d7cc3f3cdd7bb
alvarezdiego-cmyk <dr425979@gmail.com>
2026-01-16 11:42:49 -0600

    Entrega1: Area y Volumen

--- src/main.c
+++ src/main.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 
-int main(int argc, char *argv)
-{
-    printf("Hello world!\n");
+int main() {
+    float lado, area, volumen;
+
+    printf("Ingresa la longitud del lado: ");
+    scanf("%f", &lado);
+
+    area = lado * lado;
+    volumen = lado * lado * lado;
+
+    printf("El area del cuadrado es: %.2f\n", area);
+    printf("El volumen del cubo es: %.2f\n", volumen);
+
     return 0;
 }
+//
+
